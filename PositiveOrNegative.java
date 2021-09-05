6bf2dcd0796729cf5271b611304bba1439a647a1
SiddhuSid18 <90108693+SiddhuSid18@users.noreply.github.com>
2021-09-04 23:45:51 -0400

    Create PositiveOrNegative.java
    Created by Siddharth

---
+++ PositiveOrNegative.java
@@ -0,0 +1,21 @@
+package Week1.day1.Assignment1;
+
+public class Assignment1PositiveORNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 0;
+		if(number>0) {
+			System.out.println("The Given number " + number + " is Positive");
+		}
+		else if(number < 0) 
+		{
+			System.out.println("The Given number " + number + " is Negative");
+		}
+		else
+		{
+			System.out.println("The Given number " + number + " is Neither Positive nor Negative");
+		}
+	}
+
+}
